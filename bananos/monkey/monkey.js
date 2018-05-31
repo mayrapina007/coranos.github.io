b9a590ac8d5e2400e72be26a030a464775df46fe
dps <dps@localhost>
2018-05-31 11:11:24 -0500

    adding game.

--- bananos/monkey/monkey.js
+++ bananos/monkey/monkey.js
@@ -1,7 +1,29 @@
+const expectedValueImageSize = 600;
+const choiceValueImageSize = 150;
+
 const maxTime = 2000;
 var time = maxTime;
 
-var getUrlParameter = function getUrlParameter (sParam) {
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function getUrlParameter (sParam) {
   var sPageURL = decodeURIComponent(window.location.search.substring(1)), sURLVariables = sPageURL.split('&'), sParameterName, i;
 
   for (i = 0; i < sURLVariables.length; i++) {
@@ -24,7 +46,8 @@ function submitForm () {
     timeDiv.style['background-color'] = 'red';
     return false;
   }
-  return true;
+  newGame();
+  return false;
 }
 
 function updateTime () {
@@ -53,11 +76,49 @@ function updateTime () {
   }
 }
 
-function makeGame(options) {
-  d3.select(options.gameSelector).html('');
+function makeMonkeySvg (gameSelector, images, svgSize, choiceIx) {
+  let svgParent = d3.select(gameSelector);
+  if (choiceIx !== undefined) {
+    const label = d3.select(gameSelector).append('label').attr('class', 'choiceLabel')
+    label.append('input').attr('class', 'image').attr('type', 'submit').attr('name', 'choice').attr('value', choiceIx);
+
+    svgParent = label;
+  }
+
+  const svg = svgParent.append('svg').attr('width', svgSize).attr('height', svgSize);
+
+  if (choiceIx === undefined) {
+    svg.attr('class', 'expected');
+  } else {
+    svg.attr('class', 'choice');
+  }
+
+  svg.append('rect').attr('x', 1).attr('y', 1).attr('height', svgSize - 1).attr('width', svgSize - 1).style('stroke', 'black').style('fill', 'none').style(
+      'stroke-width', 1);
+
+  svg.append('image').attr('xlink:href', images.prefix + images.left).attr('x', 0).attr('y', 0).attr('height', svgSize / 2).attr('width', svgSize / 2);
+
+  svg.append('image').attr('xlink:href', images.prefix + images.right).attr('x', svgSize / 2).attr('y', svgSize / 2).attr('height', svgSize / 2).attr('width',
+      svgSize / 2);
+
+  svg.append('rect').attr('x', 1).attr('y', 1).attr('height', svgSize - 1).attr('width', svgSize - 1).attr('pointer-events', 'visible').style('stroke', 'red')
+      .style('fill', 'none').style('stroke-width', 1);
+
+}
+
+let newGame;
+
+function makeGame (options) {
+  newGame = function() {
+    makeGame (options);
+  };
   
+  d3.select('.choiceLabel').style('display','none');
+  
+  d3.select(options.gameSelector).html('');
+
   const numberOfGames = options.numberOfGames;
-  d3.select(options.gameSelector).style('width',width*Math.sqrt(numberOfGames));
+  d3.select(options.gameSelector).style('width', expectedValueImageSize);
 
   $.ajaxSetup({
     beforeSend : function (xhr) {
@@ -68,26 +129,32 @@ function makeGame(options) {
   });
 
   $.getJSON('game.json', function (gameJson) {
-    makeMonkeySvg(gameJson.expected);
-    
-    for (let choiceIx = 0; choiceIx < gameJson.choices.length; choiceIx++) {
-      makeMonkeySvg(gameJson.choices[choiceIx]);
+    if(gameJson.slowDownFlag === false) {
+      d3.select('#slowDownFlag').style('display','none');
     }
     
-    const realIx = getRandomInt(numberOfGames);
-    goodIx = realIx;
+    gameJson.expected.prefix = gameJson.prefix;
+    makeMonkeySvg(options.gameSelector, gameJson.expected, expectedValueImageSize, undefined);
 
-    for (let gameIx = 0; gameIx < numberOfGames; gameIx++) {
-      const breakHamiltonianCycle = !(gameIx == realIx);
-      const rotation = (2.0 * Math.PI) / ((gameIx * 1.0) / numberOfGames);
-      makeGameSvg(gameIx, options, bananoJson, rotation, breakHamiltonianCycle);
+    shuffle(gameJson.choices);
+    for (let choiceIx = 0; choiceIx < gameJson.choices.length; choiceIx++) {
+      gameJson.choices[choiceIx].prefix = gameJson.prefix;
+      makeMonkeySvg(options.gameSelector, gameJson.choices[choiceIx], choiceValueImageSize, choiceIx);
     }
+
+    time = maxTime
     setInterval(updateTime, 100);
     updateTime();
   });
 }
 
-function onLoad () {
+function synchAccountDisplay () {
   var account = getUrlParameter('account');
-  document.getElementById('account').innerHTML = account;
+  d3.select('#account-text').html(account);  
+  if(account === undefined) {
+    d3.select('#hasAccountFlagYes').style('display','none');
+  } else {
+    d3.select('#hasAccountFlagNo').style('display','none');
+  }
+  
 }
